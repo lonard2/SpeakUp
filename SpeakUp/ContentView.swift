cd523832d7f73bd30908b61019058ac6ffbdd8d3
Lonard Steven <lonardnew@gmail.com>
2024-05-20 17:45:32 +0700

    Initial Commit

---
+++ SpeakUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeakUp
+//
+//  Created by Lonard Steven on 20/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeakUp/SpeakUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakUpApp.swift
+//  SpeakUp
+//
+//  Created by Lonard Steven on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeakUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
